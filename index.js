c07d9a922ca00dde081aabae87d6dd61de8fbf5f
TanvirRahman888 <tanvirrahman8888@gmail.com>
2025-06-26 03:10:28 +0600

    Removed to cart

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
+const { ObjectId } = require('mongodb');
+
 
 app.use(cors());
 app.use(express.json())
@@ -33,11 +35,28 @@ async function run() {
         // add user to database
         app.post('/user', async (req, res) => {
             const user = req.body;
-            console.log("New User : ", user);
-            const result = await usersCollection.insertOne(user)
-            res.send(result)
+            const existingUser = await usersCollection.findOne({ email: user.email });
 
+            if (existingUser) {
+                return res.send({ message: 'User already exists' });
+            }
+
+            const result = await usersCollection.insertOne(user);
+            res.send(result);
         });
+
+        // Get all users
+        app.get('/users', async (req, res) => {
+            try {
+                const users = await usersCollection.find().toArray();
+                res.send(users);
+            } catch (error) {
+                console.error("Failed to fetch users:", error);
+                res.status(500).send({ error: "Failed to fetch users" });
+            }
+        });
+
+
         //add products in cart
         app.post('/cartitems', async (req, res) => {
             const cart = req.body;
@@ -45,6 +64,42 @@ async function run() {
             const result = await cartItems.insertOne(cart)
             res.send(result)
         });
+
+        // Get cart items for a user
+        app.get('/cartitems', async (req, res) => {
+            const userEmail = req.query.email;
+            if (!userEmail) return res.status(400).send({ error: 'Email required' });
+
+            const items = await cartItems.find({ userEmail }).toArray();
+            res.send(items);
+        });
+
+
+        // DELETE cart item by _id
+        app.delete('/cartitems/:id', async (req, res) => {
+            const id = req.params.id;
+
+            try {
+                const result = await cartItems.deleteOne({ _id: new ObjectId(id) });
+
+                if (result.deletedCount > 0) {
+                    res.send({ success: true, message: "Item deleted successfully" });
+                } else {
+                    res.status(404).send({ success: false, message: "Item not found" });
+                }
+            } catch (err) {
+                console.error(err);
+                res.status(500).send({ success: false, message: "Invalid ID format" });
+            }
+        });
+
+
+
+
+
+
+
+
         // Get all products
         app.get('/products', async (req, res) => {
             const products = await productsCollection.find().toArray();
